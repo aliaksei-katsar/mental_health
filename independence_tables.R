da650bf4bbc40e018de5d365f796479db786f5e0
macbookair <evgenia@vardanian.info>
2024-02-03 20:30:45 +0100

    chi2 independence tests (separarte factors+total mental wellness)

---
+++ independence_tables.R
@@ -0,0 +1,36 @@
+
+
+#Creating 2 empty independence tables for p-value and X^2 value
+p_value_table_independence <-
+  matrix(NA, nrow = ncol(MentalHealth), ncol = ncol(MentalHealth))
+
+rownames(p_value_table_independence) <-
+  colnames(p_value_table_independence) <-
+  colnames(MentalHealth)
+
+
+Xsq_value_table_independence <- 
+  matrix(NA, nrow = ncol(MentalHealth), ncol = ncol(MentalHealth))
+
+rownames(Xsq_value_table_independence) <- 
+  colnames(Xsq_value_table_independence) <- 
+  colnames(MentalHealth)
+
+
+#Making chisq test for each pair of factors and deciding if they are independent
+for (i in 1:(ncol(MentalHealth) - 1)) {
+  for (j in (i + 1):ncol(MentalHealth)) {
+    
+    chi2_result <- chisq.test(MentalHealth[[i]], 
+                                     MentalHealth[[j]])
+    
+    p_value_table_independence[i, j] <- chi2_result$p.value < 0.05
+    p_value_table_independence[j, i] <- p_value_table_independence[i, j]
+    
+    Xsq_value_table_independence[i, j] <- chi2_result$statistic > qchisq(0.95, chi2_result$parameter, lower.tail = TRUE)
+    Xsq_value_table_independence[j, i] <- Xsq_value_table_independence[i, j]
+  }
+}
+
+
+
---
+++ mental_health_wellness_independence.R
@@ -0,0 +1,28 @@
+
+
+library(tidyverse)
+MentalHealth = read_csv("MentalHealthDataSet.csv")
+
+#Finding total mental health wellness of each person by calculation the amount of bad factors that he/she has
+MentalHealth_wellness <- MentalHealth %>% 
+  mutate(Mental_Wellness = rowSums(select(., -Coping_Struggles, -Work_Interest, -Mental_Health_History, -Changes_Habits) == "Yes") +
+                         rowSums(select(.,Coping_Struggles,Work_Interest) == "No") + 
+                         rowSums(select(., Mood_Swings) == "High") +
+                         rowSums(. == "Maybe") / 2 +
+                         rowSums(select(., Mood_Swings) == "Medium") / 2)
+
+#Making chisq and fisher tests
+chisq.test(MentalHealth_wellness$Age, MentalHealth_wellness$Mental_Wellness)
+fisher.test(MentalHealth_wellness$Age, MentalHealth_wellness$Mental_Wellness, simulate.p.value = TRUE)
+
+chisq.test(MentalHealth_wellness$Gender, MentalHealth_wellness$Mental_Wellness)
+fisher.test(MentalHealth_wellness$Gender, MentalHealth_wellness$Mental_Wellness, simulate.p.value = TRUE)
+
+chisq.test(MentalHealth_wellness$Occupation, MentalHealth_wellness$Mental_Wellness)
+fisher.test(MentalHealth_wellness$Occupation, MentalHealth_wellness$Mental_Wellness, simulate.p.value = TRUE)
+
+chisq.test(MentalHealth_wellness$Days_Indoors, MentalHealth_wellness$Mental_Wellness)
+fisher.test(MentalHealth_wellness$Days_Indoors, MentalHealth_wellness$Mental_Wellness, simulate.p.value = TRUE)
+
+chisq.test(MentalHealth_wellness$Mental_Health_History, MentalHealth_wellness$Mental_Wellness)
+fisher.test(MentalHealth_wellness$Mental_Health_History, MentalHealth_wellness$Mental_Wellness, simulate.p.value = TRUE)
